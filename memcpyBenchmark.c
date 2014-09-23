865c561aeb7f707df801438bf7d9b1af119dfe3f
GirijaO <girij.ogale@gmail.com>
2014-09-23 16:31:05 -0500

    Update memcpyBenchmark.c

--- memcpyBenchmark.c
+++ memcpyBenchmark.c
@@ -6,7 +6,7 @@
 
 #define BLOCK_SIZE 1
 #define BLOCK_SIZE_KB 1024
-#define BLOCK_SIZE_MB 1024*1024
+#define BLOCK_SIZE_MB 1024*1000
 
 void *block_Byte();
 void *block_Kbyte();
@@ -20,7 +20,7 @@ int main()
 
 	struct timeval start, end;
 	struct timezone tzp;
-
+	clock_t start_t, end_t, total_t=0,start_t1, end_t1,total_t1=0;
 	double latency,throughput;
 	printf("\n........Program to find Memory Benchmark.......\n");
 	pthread_t th[10];// array of threads
@@ -28,10 +28,10 @@ int main()
 	int ch,nthread;
 	while(1)
 	{
-		printf("\nEnter the Block Size:\n1.Sequential Byte Operation\n2.Random Byte Operation \n3.Sequential KiloByte Operation\n4.Random KiloByte Operation\n5.Sequential MegaByte Operation\n6.Random MegaByte Operation\n7.Exit \nEnter your choice: ");
+		printf("\nEnter the Block Size:\n1.BYTE\n2.KILOBYTE\n3.MEGABYTE\n4.Exit\n");
 
 		scanf("%d",&ch);
-		if(ch>=7)
+		if(ch>=4)
 		{
 			exit(0);
 		}
@@ -39,113 +39,142 @@ int main()
 		scanf("%d",&nthread);
 		switch (ch)
 		{
-			case 1: //Sequential Memroy Read+Write
+			case 1: 
+			
+			//Sequential Memroy Read+Write
+			
 			printf("\nByte read+write for %d thread",nthread);
 			printf("\n\n..Sequential Read+Write..\n");
 			printf("-------------------------------------\n");
-			gettimeofday(&start, &tzp);
+		
 			for(i=0;i<nthread;i++)
 			{
+			
+				start_t = clock();	
 				pthread_create(&th[i],NULL,block_Byte,NULL);
 				pthread_join(th[i], NULL);
+				total_t += clock() - start_t;
 			}
-			gettimeofday(&end, &tzp);
-			latency= (end.tv_usec - start.tv_usec)/(double)(nthread*1000);
-			printf("LATENCY : %f ms\n",latency);
-			throughput=(BLOCK_SIZE/(double)((latency)*1000));
-			printf("THROUGHPUT : %f MB/sec\n",throughput);
-			break;
-
-			case 2:
+			
+			double lat = (double)(total_t*1000)/(nthread*CLOCKS_PER_SEC);
+			printf("Latency for sequential 1 Byte : %f\n", lat);
+			total_t = 0;
+		
+			throughput=(BLOCK_SIZE/(double)((lat)*100*1000));
+			printf("THROUGHPUT for seq 1 Byte : %f MB/sec\n",throughput);
+			lat =0;
+			
 			// Random Memroy Read+Write
+			
 			printf("..Random read+write..\n");
 			printf("-------------------------------------\n");
-			gettimeofday(&start, &tzp);
 			for(i=0;i<nthread;i++)
 			{
+				start_t1 = clock();
 				pthread_create(&th[i],NULL,block_Byte_random,NULL);
 				pthread_join(th[i], NULL);
+				total_t1 += clock() - start_t1;
 			}
-			gettimeofday(&end, &tzp);
-			latency= (end.tv_usec - start.tv_usec)/(double)(nthread*1000);
-			printf("LATENCY : %f ms\n",latency);
-			throughput=(BLOCK_SIZE/(double)((latency)*1000));
-			printf("THROUGHPUT:%f MB/sec\n",throughput);
-
-
+		
+			double lat1 = (double)(total_t1*1000)/(nthread*CLOCKS_PER_SEC);
+			printf("Latency for random 1 Byte :%f\n", lat1);
+		
+			throughput=(BLOCK_SIZE/(double)((lat1)*100*1000));
+			printf("THROUGHPUT for 1 byte random : %f MB/sec\n",throughput);
+			
 			break;
 
-			case 3: //Sequential Memroy Read+Write
+			case 2: 
+			
+			//Sequential Memroy Read+Write for KILOBYTE block
+			
 			printf("\nKiloByte read+write for %d thread ",nthread);
 			printf("\n\n..Sequential Read+Write..\n");
 			printf("-------------------------------------\n");
-			gettimeofday(&start, &tzp);
+			
 			for(i=0;i<nthread;i++)
 			{
+				start_t = clock();
 				pthread_create(&th[i],NULL,block_Kbyte,NULL);
 				pthread_join(th[i], NULL);
+				total_t += clock() - start_t;
 			}
-			gettimeofday(&end, &tzp);
-			latency= (end.tv_usec - start.tv_usec)/(double)(nthread*1000);
-			printf("LATENCY : %f ms\n",latency);
-			throughput=(BLOCK_SIZE_KB/(double)((latency)*1000));
+			
+			double lat = (double)(total_t*1000)/(nthread*CLOCKS_PER_SEC);
+			printf("Latency for sequential 1 Byte : %f\n", lat);
+			total_t = 0;
+			
+			throughput=(BLOCK_SIZE_KB/(double)((lat)*100*1000));
 			printf("THROUGHPUT : %f MB/sec\n",throughput);
-			break;
-
-			case 4:
-			//Random Memroy Read+Write
+			
+			//Random Memroy Read+Write for KILOBYTE block
+			
 			printf("..Random read+write..\n");
 			printf("-------------------------------------\n");
-			gettimeofday(&start, &tzp);
+			
 			for(i=0;i<nthread;i++)
 			{
+				start_t1 = clock();
 				pthread_create(&th[i],NULL,block_KByte_random,NULL);
 				pthread_join(th[i], NULL);
+				total_t1 += clock() - start_t1;
 			}
-			gettimeofday(&end, &tzp);
-			latency= (end.tv_usec - start.tv_usec)/(double)(nthread*1000);
-			printf("LATENCY : %f ms\n",latency);
-			throughput=(BLOCK_SIZE_KB/(double)((latency)*1000));
+			
+			double lat1 = (double)(total_t1*1000)/(nthread*CLOCKS_PER_SEC);
+			printf("Latency for sequential 1 Byte : %f\n", lat);
+			total_t = 0;
+			
+			throughput=(BLOCK_SIZE_KB/(double)((lat1)*100*1000));
 			printf("THROUGHPUT : %f MB/sec\n",throughput);
 	
 			break;
 
-			case 5: //Sequential Memroy Read+Write
+			case 3: 
+			
+			//Sequential Memroy Read+Write
+			
 			printf("\nMegaByte read+write for %d thread \n",nthread);
 			printf("..Sequential Read+Write..\n");
 			printf("-------------------------------------\n");
-			gettimeofday(&start, &tzp);
+			
 			for(i=0;i<nthread;i++)
 			{
+				start_t = clock();
 				pthread_create(&th[i],NULL,block_Mbyte,NULL);
 				pthread_join(th[i], NULL);
+				total_t += clock() - start_t;
 			}
-			gettimeofday(&end, &tzp);
-			latency= (end.tv_usec - start.tv_usec)/(double)(nthread*1000);
-			printf("LATENCY : %f ms\n",latency);
-			throughput=(BLOCK_SIZE_MB/(double)((latency)*1000));
+			
+			double lat = (double)(total_t*1000)/(nthread*CLOCKS_PER_SEC);
+			printf("Latency for sequential 1 Byte : %f\n", lat);
+			total_t = 0;
+			
+			throughput=(BLOCK_SIZE_MB/(double)((lat)*100*1000));
 			printf("THROUGHPUT : %f MB/sec\n",throughput);
-			break;
-
-			case 6:
+			
 			//Random Memory Read+Write
+			
 			printf("..Random read+write..\n");
 			printf("-------------------------------------\n");
-			gettimeofday(&start, &tzp);
+			
 			for(i=0;i<nthread;i++)
 			{
+				start_t1 = clock();
 				pthread_create(&th[i],NULL,block_MByte_random,NULL);
 				pthread_join(th[i], NULL);
+				total_t1 += clock() - start_t1;
 			}
-			gettimeofday(&end, &tzp);
-			latency= (end.tv_usec - start.tv_usec)/(double)(nthread*1000);
-			printf("LATENCY : %f ms\n",latency);
-			throughput=(BLOCK_SIZE_MB/(double)((latency)*1000));
+			
+			double lat1 = (double)(total_t1*1000)/(nthread*CLOCKS_PER_SEC);
+			printf("Latency for sequential 1 Byte : %f\n", lat);
+			total_t = 0;
+			
+			throughput=(BLOCK_SIZE_MB/(double)((lat1)*100*1000));
 			printf("THROUGHPUT : %f MB/sec\n",throughput);
 	
 			break;
 
-			case 7: exit(0);
+			case 4: exit(0);
 			break;
 			default:printf("\nPlease enter a valid option..\n");	
 		}
@@ -154,20 +183,22 @@ int main()
 
 void *block_Byte()
 {
-	
 	int i;
 	int len;
 	double a=5;
-	char mem = 'C';	
-	char *mem_write=malloc(sizeof(BLOCK_SIZE));
-
-	for(i=0;i<BLOCK_SIZE;i++)
+	long k= 0;
+	char *mem = malloc(100*sizeof(*mem));
+	for(k=0;k<100;k++)
+	{
+		mem[k]='c';
+	}	
+	char *mem_write=malloc(100*sizeof(*mem_write));
+		
+	for(i=0;i<100;i++)
 	{
 		memcpy(mem_write,&mem,BLOCK_SIZE);
 		*(mem_write+i);
 	}
-
-	free(mem_write);
 }
 
 void *block_Byte_random()
@@ -176,19 +207,20 @@ void *block_Byte_random()
 	int i,r;
 	int len;
 	double a=5;
-	char mem = 'C';	
-	char *mem_write=malloc(sizeof(BLOCK_SIZE));
+	long k= 0;
 	
-//	srand((unsigned)&mem_write);
-
-	for(i=0;i<BLOCK_SIZE;i++)
+	char *mem = malloc(100*sizeof(*mem));
+	for(k=0;k<100;k++)
 	{
-		r = rand()%1000000;
-		memcpy(mem_write,&mem,BLOCK_SIZE);
-		*(mem_write+r);
+		mem[k]='c';
+	}	
+	char *mem_write=malloc(100*sizeof(*mem_write));
+		
+	for(i=0;i<100;i++)
+	{
+		r = rand()%100;	
+		memcpy(mem_write + r,&mem +r,BLOCK_SIZE);
 	}
-
-	free(mem_write);
 }
 
 void *block_Kbyte()
@@ -197,36 +229,40 @@ void *block_Kbyte()
 	int i;
 	int len;
 	double a=5;
-	char mem = 'C';	
-	char *mem_write=malloc(sizeof(BLOCK_SIZE_KB));
+	long k= 0;
+	char *mem = malloc(100*1024*sizeof(*mem));
 	
-	for(i=0;i<BLOCK_SIZE_KB;i++)
+	for(k=0;k<100*1024;k++)
 	{
-		memcpy(mem_write,&mem,BLOCK_SIZE);
+		mem[k]='c';
+	}	
+	char *mem_write=malloc(100*1024*sizeof(*mem_write));
+		
+	for(i=0;i<100;i++)
+	{
+		memcpy(mem_write,&mem,BLOCK_SIZE_KB);
 		*(mem_write+i);
 	}
-
-	free(mem_write);
-
 }
 
 void *block_KByte_random()
 {
-	
 	int i,r;
 	int len;
 	double a=5;
-	char mem = 'C';	
-	char *mem_write=malloc(sizeof(BLOCK_SIZE_KB));
-
-	for(i=0;i<BLOCK_SIZE_KB;i++)
+	long k= 0;
+	char *mem = malloc(100*1024*sizeof(*mem));
+	for(k=0;k<100*1024;k++)
 	{
-		r = rand()%10;	
-		memcpy(mem_write,&mem,BLOCK_SIZE);
-		*(mem_write+r);
+		mem[k]='c';
+	}	
+	char *mem_write=malloc(100*1024*sizeof(*mem_write));
+		
+	for(i=0;i<100;i++)
+	{
+		r = rand()%100*1024;	
+		memcpy(mem_write + r,&mem +r,BLOCK_SIZE_KB);	
 	}
-
-	free(mem_write);
 }
 
 void *block_Mbyte()
@@ -234,17 +270,19 @@ void *block_Mbyte()
 	int i;
 	int len;
 	double a=5;
-	char mem = 'C';	
-	char *mem_write=malloc(sizeof(BLOCK_SIZE_MB));
-
-	for(i=0;i<BLOCK_SIZE_MB;i++)
+	long k= 0;
+	char *mem = malloc(100*1024*1024*sizeof(*mem));
+	for(k=0;k<1024*1024*100;k++)
 	{
-		memcpy(mem_write,&mem,BLOCK_SIZE);
+		mem[k]='c';
+	}	
+	char *mem_write=malloc(100*1024*1024*sizeof(*mem_write));
+		
+	for(i=0;i<1024*1024*100;i++)
+	{
+		memcpy(mem_write,&mem,BLOCK_SIZE_MB);
 		*(mem_write+i);
 	}
-
-	free(mem_write);
-
 }
 
 void *block_MByte_random()
@@ -253,16 +291,18 @@ void *block_MByte_random()
 	int i,r;
 	int len;
 	double a=5;
-	char mem = 'C';	
-	char *mem_write=malloc(sizeof(BLOCK_SIZE_MB));
-
-	for(i=0;i<BLOCK_SIZE_MB;i++)
+	long k= 0;
+	char *mem = malloc(100*1024*1024*sizeof(*mem));
+	for(k=0;k<1024*1024*100;k++)
 	{
-		r = rand()%10;	
-		memcpy(mem_write,&mem,BLOCK_SIZE);
-		*(mem_write+r);
-	}
+		mem[k]='c';
+	}	
+	char *mem_write=malloc(100*1024*1024*sizeof(*mem_write));
 
-	free(mem_write);
+	for(i=0;i<100;i++)
+	{
+		r = rand()%100*1024*1024;	
+		memcpy(mem_write + r,&mem +r,BLOCK_SIZE_MB);
+	}
 }
 
